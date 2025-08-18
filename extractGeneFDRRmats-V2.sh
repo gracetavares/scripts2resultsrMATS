52c3f18aa87edbe2551adfa359cae9c28f06e6d3
gracetavares <grace.biologia@gmail.com>
2025-08-18 19:13:27 -0300

    fix: correção de bug no threshold

--- extractGeneFDRRmats-V2.sh
+++ extractGeneFDRRmats-V2.sh
@@ -36,15 +36,15 @@ for INPUT_FILE in "$INPUT_DIR"/*.txt; do
     
     # Filtrar pelo FDR < 0.05 e valores altos de IncLevelDifference (próximos de 1 ou -1)
     #awk -F "\t" 'NR==1 || ($19 < 0.05 && ($21 >= 0.8 || $21 <= -0.8))' "$INPUT_FILE" > "$OUTPUT_FILE"
-     #awk -F "\t" threshold="$DIFF_THRESHOLD" 'NR==1 || ($19 < 0.05 && ($21 >= threshold || $21 <= -threshold))' "$INPUT_FILE" > "$OUTPUT_FILE"
+    awk -F "\t" threshold="$DIFF_THRESHOLD" 'NR==1 || ($19 < 0.05 && ($21 >= threshold || $21 <= -threshold))' "$INPUT_FILE" > "$OUTPUT_FILE"
     # Pega índices das colunas FDR e IncLevelDifference no cabeçalho
-        FDR_COL=$(head -1 "$INPUT_FILE" | tr '\t' '\n' | grep -n "^FDR$" | cut -d: -f1)
-        DIFF_COL=$(head -1 "$INPUT_FILE" | tr '\t' '\n' | grep -n "^IncLevelDifference$" | cut -d: -f1)
-        
+        #FDR_COL=$(head -1 "$INPUT_FILE" | tr '\t' '\n' | grep -n "^FDR$" | cut -d: -f1)
+        #DIFF_COL=$(head -1 "$INPUT_FILE" | tr '\t' '\n' | grep -n "^IncLevelDifference$" | cut -d: -f1)
+        echo $DIFF_COL
         # Filtrar os dados com base no FDR e no limiar de diferença
-            awk -v fdr_cut="$FDR_CUTOFF" -v diff_cut="$DIFF_CUTOFF" -v fdr_col="$FDR_COL" -v diff_col="$DIFF_COL" \
-            'BEGIN {OFS="\t"} NR==1 || ($fdr_col <= fdr_cut && ($diff_col >= diff_cut || $diff_col <= -diff_cut))' \
-            "$INPUT_FILE" > "$OUTPUT_FILE"
+            #awk -v fdr_cut="$FDR_CUTOFF" -v diff_cut="$DIFF_CUTOFF" -v fdr_col="$FDR_COL" -v diff_col="$DIFF_COL" \
+            #'BEGIN {OFS="\t"} NR==1 || ($fdr_col <= fdr_cut && ($diff_col >= diff_cut || $diff_col <= -diff_cut))' \
+            #"$INPUT_FILE" > "$OUTPUT_FILE"
     echo "Arquivo filtrado: $OUTPUT_FILE"
 done
 
